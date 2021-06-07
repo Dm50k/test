e1e74fd710bfe19b5eec44e641a0bddba60de1b3
Deepak <deepak97n@gmail.com>
2021-06-07 18:01:18 +0530

    multiply

--- hungry.js
+++ hungry.js
@@ -2,4 +2,10 @@
 let a = 5 
 let b = 6
 result = a + b
-console.log(`addition of ${a} and ${b}is ${result}`)
+console.log(`addition of ${a} and  ${b}is ${result}`)
+
+
+let a1 = 5 
+let b1 = 6 
+result1 = a1*b1
+console.log(`addition of ${a} and  ${b}is ${result}`)
