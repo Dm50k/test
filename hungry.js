a4c57af8daf1fab9ad44a9175a0b64b7c403a582
Deepak <deepak97n@gmail.com>
2021-06-07 18:23:31 +0530

    division done[C

--- hungry.js
+++ hungry.js
@@ -8,4 +8,16 @@ console.log(`addition of ${a} and  ${b}is ${result}`)
 let a1 = 5 
 let b1 = 6 
 result1 = a1*b1
-console.log(`addition of ${a} and  ${b}is ${result}`)
+console.log(`Multiplication of ${a} and  ${b}is ${result}`)
+
+
+
+
+
+let c1 = 5 
+let d1 = 6 
+result1 = c1/d1
+console.log(`division of ${c1} and  ${d1}is ${result1}`)
+
+
+
